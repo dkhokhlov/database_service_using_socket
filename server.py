e1fe57e7ad962ddc259615c9a22ad00a8c158846
Dmitri Khokhlov <dkhokhlov@gmail.com>
2021-11-11 00:55:04 -0800

    extracted response & request func

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import asyncio
 import signal
+import urllib.parse
 
 import constants as const
 
@@ -32,7 +33,6 @@ async def serve():
         s.setblocking(False)
 
         loop = asyncio.get_event_loop()
-
         while True:
             conn, addr = await loop.sock_accept(s)
             print("Connected by", addr)
@@ -40,28 +40,60 @@ async def serve():
             loop.create_task(handle_connection(conn, addr, loop))
 
 
+def compose_response(status: int, json: str = '') -> bytes:
+    content = json.encode("utf-8")
+    length = len(content)
+    header = f'HTTP/1.1 {status}\n'
+    header += 'Connection: keep-alive\n'
+    if json:
+        header += 'Content-Type: application/json; charset=utf-8\n'
+        header += 'Cache-control: no-cache, no-store, must-revalidate\n'
+    header += f'Content-Length: {length}\n\n'
+    response = header.encode("utf-8") + content
+    return response
+
+
+async def handle_request(request: str) -> bytes:
+    lines = request.split("\n")
+    method, req_path, version = lines[0].split(" ")
+    url = urllib.parse.urlparse(req_path)
+    query = urllib.parse.parse_qs(url.query)
+    path = url.path
+    if version == "HTTP/1.0":
+        response = compose_response(505)  # HTTP Version not supported
+        return response
+    if "content-length:" in request.lower():
+        response = compose_response(400)  # Bad Request
+        return response
+    if path == "/ping":
+        await delay()
+        response = compose_response(200)  # OK
+    else:
+        response = compose_response(404)  # Not Found
+    return response
+
+
 async def handle_connection(conn_sock, addr, loop):
-    print(f"handle_connection[{conn_sock.fileno()}]: begin", addr)
+    print(f"handle_connection {addr}: begin")
     with conn_sock:
+        buffer = ''
         while True:
             data = await loop.sock_recv(conn_sock, 2048)  # assume all requests are smaller than 2048 bytes
-            raw = data.decode("utf-8")
-            lines = raw.split("\r\n")
-            request = "  \n".join(lines)
-            print(f"Request: \n{request}")
-
-            method, path, version = lines[0].split(" ")
-            if path == "/ping":
-                await delay()
-                response = "HTTP/1.1 200 Ok\n\n"
-            else:
-                response = "xxx"
-
-
-            print(f"Response: \n{response}")
-            await loop.sock_sendall(conn_sock, response.encode("utf-8"))
-            print(f"handle_connection[{conn_sock.fileno()}]: end", addr)
-            break
+            if data == b'':
+                break  # connection closed
+            data_str = data.decode("utf-8")
+            data_str = data_str.replace("\r", "")
+            buffer += data_str
+            end_of_header = buffer.find('\n\n')
+            if end_of_header < 0:
+                continue
+            request = buffer[:end_of_header]
+            buffer = buffer[end_of_header + 2:]
+            print(f"Request: \n{request}\n\n")
+            response = await handle_request(request)
+            print(f"Response: \n{response.decode('utf-8')}")
+            await loop.sock_sendall(conn_sock, response)
+    print(f"handle_connection {addr}: closed")
 
 
 async def delay():
