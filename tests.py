fba5c8e16e1f57828ec91db36d62d7c597d4c615
Dmitri Khokhlov <dkhokhlov@gmail.com>
2021-11-12 03:22:09 -0800

    test delete + insert

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from http import client
 import constants
 import urllib
 import json
+import utils
 
 
 def catch(func):
@@ -37,8 +38,12 @@ def test(
     assert (response_body and expect_body) or (not response_body and not expect_body)
 
     if expect_fields:
-        # parse json to dict and look for key/value
-        ...
+        if "SSN" in expect_fields.keys():
+            expect_fields['SSN'] = utils.encode_SSN(expect_fields['SSN'])
+        json_obj = json.loads(response_body)
+        for k in expect_fields.keys():
+            assert(json_obj[0][k] == expect_fields[k])
+
     print(f"Success({name})!\n")
 
 
@@ -54,13 +59,25 @@ def test_single_insert():
         json_str = f.read()
         query = json.loads(json_str)
         query_enc = urllib.parse.urlencode(query)
-        test("InsertTest", path=f"/pii/insert?{query_enc}", method="PUT", expected_response=400, expect_body=True)
+        test("InsertTest", path=f"/pii/insert?{query_enc}", method="PUT",
+             expected_response=200, expect_body=True, expect_fields=query)
+
+@catch
+def test_single_delete():
+    print("InsertTest")
+    with open("data/pb.json", "r") as f:
+        json_str = f.read()
+        query = json.loads(json_str)
+        query_enc = urllib.parse.urlencode(query)
+        test("DeleteTest", path=f"/pii/delete?{query_enc}", method="DELETE",
+             expected_response=200, expect_body=True, expect_fields=query)
 
 
 def main():
     tests = [
         test_ping,
         test_single_insert,
+        test_single_delete,
     ]
     for test in tests:
         test()
